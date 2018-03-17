824ab2561ac38e9956ee3b62da2509a71b174e69
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-17 08:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function takeANumber(katzDeliLine, name){
 function nowServing(deliLine){
   if(deliLine.length > 0){
     var firstInLine = deliLine.shift()
-    return `string text`
+    return `Currently serving ${firstInLine}.`
   }
+  else
+    return ""
     
 }
