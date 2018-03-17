381fbc9e1f4addbc59a2f4970da0a298016617d6
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-17 08:09:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(deliLine){
+  
+}
